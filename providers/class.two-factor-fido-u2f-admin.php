e1af3fad85dce2a93aec8c2f983cf3fca40f10a4
Jon <jjones646@gmail.com>
2016-03-28 06:42:19 -0400

    updating

--- providers/class.two-factor-fido-u2f-admin.php
+++ providers/class.two-factor-fido-u2f-admin.php
@@ -128,7 +128,6 @@ class Two_Factor_FIDO_U2F_Admin {
 		?>
 		<div class="security-keys" id="security-keys-section">
 			<div class="register-security-key">
-
 				<?php if ( Two_Factor_FIDO_U2F::is_browser_support() ) : // @todo do this in JS instead ?>
 				<input type="hidden" name="do_new_security_key" id="do_new_security_key" />
 				<input type="hidden" name="u2f_response" id="u2f_response" />
@@ -147,20 +146,18 @@ class Two_Factor_FIDO_U2F_Admin {
 			<?php if ( $new_key ) : // @todo toggle this in JS instead ?>
 			<p class="new-security-key"><?php esc_html_e( 'Security Key successfully registered.' ); ?></p>
 			<?php endif; ?>
-
+			<div class="two-factor-fido-u2f two-factor-toggle hide-if-js">
 			<?php
 			if ( ! empty( $security_keys ) ) {
 				require( TWO_FACTOR_DIR . 'providers/class.two-factor-fido-u2f-admin-list-table.php' );
 				$u2f_list_table = new Two_Factor_FIDO_U2F_Admin_List_Table();
 				$u2f_list_table->items = $security_keys;
 				$u2f_list_table->prepare_items();
-
-				?><div class="two-factor-fido-u2f two-factor-toggle hide-if-js"><?php
 				$u2f_list_table->display();
 				$u2f_list_table->inline_edit();
-				?></div><?php
 			}
 			?>
+		</div>
 		</div>
 		<?php
 	}
