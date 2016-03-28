9553598f18b88623dd753d0cc31d792b56f5fea2
Jon <jjones646@gmail.com>
2016-03-28 02:29:11 -0400

    formatting

--- providers/js/fido-u2f-login.js
+++ providers/js/fido-u2f-login.js
@@ -1,10 +1,10 @@
 /* global u2f, u2fL10n */
 var u2fL10n;
-( function( $ ) {
-	setTimeout( function() {
-		u2f.sign( u2fL10n.request, function( data ) {
-			$( '#u2f_response' ).val( JSON.stringify( data ) );
-			$( '#loginform' ).submit();
-		} );
-	}, 1000 );
-} )( jQuery );
+(function($) {
+    setTimeout(function() {
+        u2f.sign(u2fL10n.request, function(data) {
+            $('#u2f_response').val(JSON.stringify(data));
+            $('#loginform').submit();
+        });
+    }, 1000);
+})(jQuery);
