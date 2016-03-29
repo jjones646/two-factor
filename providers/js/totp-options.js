995b97ad98596106c3a1ee5aa90d80c770f0587e
Jon <jjones646@gmail.com>
2016-03-29 04:55:12 -0400

    updating

--- providers/js/totp-options.js
+++ providers/js/totp-options.js
@@ -1,5 +1,6 @@
 (function($) {
     $('button.two-factor-register.two-factor-totp').click(function() {
+        console.log($(this));
         $(this).toggleClass('clicked');
         $('#two-factor-totp-options').slideToggle();
     });
