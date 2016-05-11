4331778cb40af651d5ff667f6400672b6c2e48d7
Jon <jjones646@gmail.com>
2016-05-11 06:16:50 -0400

    tweaking commit

--- providers/js/u2f-login.js
+++ providers/js/u2f-login.js
@@ -1,7 +1,9 @@
 /* global u2f, u2fL10n */
 (function($) {
     setTimeout(function() {
+        console.log(u2fL10n);
         u2f.sign(u2fL10n.request, function(data) {
+            console.log(data);
             $('#u2f_response').val(JSON.stringify(data));
             $('#loginform').submit();
         });
