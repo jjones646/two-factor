6f3328385cb3deaa98cbf758a192709f4628e61c
Jon <jjones646@gmail.com>
2016-05-11 05:33:20 -0400

    tweaking commit

--- class-two-factor.php
+++ class-two-factor.php
@@ -498,7 +498,7 @@ class Two_Factor {
 
 		if ( isset( $_POST['provider'] ) ) {
 			$provider_key = $_POST[ 'provider' ];
-			wp_die( var_dump( $provider_key ) );
+			wp_die( var_dump( self::get_available_providers_for_user( $user ) ) );
 			$provider = array_filter( self::get_available_providers_for_user( $user ), function( $p ) use ( $provider_key ) {
 							return strcasecmp( $p['key'], $provider_key );
 						} );
